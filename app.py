3891a91ab27c8ed2168e0dce9df08380b1bc99e3
DevelopersSpace <65402301+DevelopersSpace@users.noreply.github.com>
2020-09-19 18:51:05 +0530

    itle settings

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+arduino='Arduino'
+homen='Developers Space'
+title=[]
 @app.route("/")
 def home():
-	return render_template("Homepage.html")
+	return render_template("Homepage.html", title=homen)
 @app.route("/Arduino")
 def ardpg():
-	return render_template("Arduino.html")
+	return render_template("Arduino.html", title=arduino)
